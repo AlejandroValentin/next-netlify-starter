38bee0b3a5011927c793e1718f95e65f93003308
VALENTIN <103033385+AlejandroValentin@users.noreply.github.com>
2023-09-17 05:52:12 -0300

    Just editing

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>STARTING!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="jujuujuj TRYING THISSSS" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Absolutely doing <code>it!!!!</code>
         </p>
       </main>
 
